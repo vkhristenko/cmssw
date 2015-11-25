4fcc6a8d84368e09ae78bce8985f208317a192af
Viktor Khristenko <vdkhristenko1991@gmail.com>
2015-11-25 20:17:40 +0100

    preping for HI. adding ZDC monitoring plots

--- DQM/HcalMonitorModule/interface/ZDCMonitorModule.h
+++ DQM/HcalMonitorModule/interface/ZDCMonitorModule.h
@@ -171,6 +171,21 @@ private:
 	bool dump2database_;
 	//std::map<HcalDetId, unsigned int> myquality_;
 	//HcalChannelQuality* chanquality_;
+
+	/*
+	 *	Modified by VK
+	 */
+
+	int	_nChs;
+	std::vector<HcalZDCDetId>  _vDetIds;
+	std::vector<MonitorElement*> _vShape;
+	std::vector<MonitorElement*> _vADC;
+	MonitorElement*				_NumDigisvsLS;
+	MonitorElement*				_NumTimeSamplesvsLS;
+
+	/*
+	 *	\Modified by VK
+	 */
 };
 
 #endif
