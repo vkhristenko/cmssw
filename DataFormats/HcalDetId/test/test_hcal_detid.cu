bc56f995082c961c4e2798e05cecb08c297a716f
Viktor Khristenko <vdkhristenko1991@gmail.com>
2018-05-29 12:22:02 +0200

    adding a check for cuda devices to the tests

--- DataFormats/HcalDetId/test/test_hcal_detid.cu
+++ DataFormats/HcalDetId/test/test_hcal_detid.cu
@@ -68,8 +68,10 @@ int main(int argc, char** argv) {
     std::cout << "nDevices = " << nDevices << std::endl;
 
     // test det id functionality
-    test_detid();
+    if (nDevices>0)
+        test_detid();
 
     // test hcal det ids
-    test_hcal_detid();
+    if (nDevices>0)
+        test_hcal_detid();
 }
