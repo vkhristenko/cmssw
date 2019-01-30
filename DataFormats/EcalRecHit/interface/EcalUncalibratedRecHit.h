55c57bef37791cb02473ab1748006d3787bbf533
Viktor Khristenko <vdkhristenko1991@gmail.com>
2019-01-30 14:20:13 +0100

    EcalUncalibratedRecHit constexpr

--- DataFormats/EcalRecHit/interface/EcalUncalibratedRecHit.h
+++ DataFormats/EcalRecHit/interface/EcalUncalibratedRecHit.h
@@ -1,9 +1,11 @@
 #ifndef DATAFORMATS_ECALUNCALIBRATEDRECHIT
 #define DATAFORMATS_ECALUNCALIBRATEDRECHIT
 
-#include <vector>
+#include <cmath>
+
 #include "DataFormats/DetId/interface/DetId.h"
 #include "DataFormats/EcalDigi/interface/EcalDataFrame.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 class EcalUncalibratedRecHit {
 
@@ -22,41 +24,131 @@ class EcalUncalibratedRecHit {
           
   };
 
-  EcalUncalibratedRecHit();
+    constexpr EcalUncalibratedRecHit() : 
+      amplitude_(0.), amplitudeError_(0.), pedestal_(0.), jitter_(0.), 
+      chi2_(10000.),
+      OOTamplitudes_{0., 0., 0., 0., 0., 0., 0., 0., 0., 0.}, 
+      OOTchi2_{0}, flags_{0}, aux_{0}
+    {}
 
   EcalUncalibratedRecHit(const DetId& id, float ampl, float ped,
-                         float jit, float chi2, uint32_t flags = 0, uint32_t aux = 0);
+                         float jit, float chi2, uint32_t flags = 0, 
+                         uint32_t aux = 0)
+      : amplitude_(ampl), amplitudeError_(0.), pedestal_(ped), 
+        jitter_(jit), chi2_(chi2), 
+        OOTamplitudes_{0., 0., 0., 0., 0., 0., 0., 0., 0., 0.}, 
+        OOTchi2_{0}, flags_{flags}, aux_{aux}, id_{id} {}
 
 
-  float amplitude() const { return amplitude_; }
-  float amplitudeError() const { return amplitudeError_; }
-  float pedestal() const { return pedestal_; }
-  float jitter() const { return jitter_; }
-  float chi2() const { return chi2_; }
-  float outOfTimeAmplitude(int bx) const { return OOTamplitudes_[bx]; }
+  constexpr float amplitude() const { return amplitude_; }
+  constexpr float amplitudeError() const { return amplitudeError_; }
+  constexpr float pedestal() const { return pedestal_; }
+  constexpr float jitter() const { return jitter_; }
+  constexpr float chi2() const { return chi2_; }
+  constexpr float outOfTimeAmplitude(int bx) const { return OOTamplitudes_[bx]; }
 
-  uint32_t flags() const { return flags_; }
-  float jitterError() const;
-  uint8_t jitterErrorBits() const;
-  DetId  id() const { return id_; }
+  constexpr uint32_t flags() const { return flags_; }
+    constexpr float jitterError() const
+    {
+            // stored in ps, but return BXs to match with jitter units
+            uint32_t jitterErrorBits = 0xFF & aux_;
+            // all bits off --> time reco bailed out (return negative value)
+            if( (0xFF & jitterErrorBits) == 0x00)
+                    return -1;
+            // all bits on  --> time error over 5 ns (return large value)
+            if( (0xFF & jitterErrorBits) == 0xFF)
+                    return 10000;
 
-  void setAmplitude( float amplitude ) { amplitude_ = amplitude; }
-  void setAmplitudeError( float amplitudeerror ) { amplitudeError_ = amplitudeerror; }
-  void setPedestal( float pedestal ) { pedestal_ = pedestal; }
-  void setJitter( float jitter ) { jitter_ = jitter; }
-  void setChi2( float chi2 ) { chi2_ = chi2; }
-  void setOutOfTimeAmplitude( int bx, float amplitude ) { OOTamplitudes_[bx] = amplitude; }
+            float LSB = 1.26008;
+            uint8_t exponent = jitterErrorBits>>5;
+            uint8_t significand = jitterErrorBits & ~(0x7<<5);
+            return (float)(std::pow(2,exponent)*significand*LSB)/(25.*1000);
+    }
+    constexpr uint8_t jitterErrorBits() const
+    {
+            uint8_t jitterErrorBits = 0xFF & aux_;
+            return jitterErrorBits;
+    }
+  constexpr DetId  id() const { return id_; }
 
-  void setJitterError( float jitterErr );
-  void setFlags( uint32_t flags ) { flags_ = flags; }
-  void setId( DetId id ) { id_ = id; }
-  void setAux( uint32_t aux ) { aux_ = aux; }
-  void setFlagBit(Flags flag);
-  bool checkFlag(Flags flag) const;
+  constexpr void setAmplitude( float amplitude ) { amplitude_ = amplitude; }
+  constexpr void setAmplitudeError( float amplitudeerror ) { amplitudeError_ = amplitudeerror; }
+  constexpr void setPedestal( float pedestal ) { pedestal_ = pedestal; }
+  constexpr void setJitter( float jitter ) { jitter_ = jitter; }
+  constexpr void setChi2( float chi2 ) { chi2_ = chi2; }
+  constexpr void setOutOfTimeAmplitude( int bx, float amplitude ) { OOTamplitudes_[bx] = amplitude; }
 
-  bool isSaturated() const;
-  bool isJitterValid() const;
-  bool isJitterErrorValid() const;
+    constexpr void setJitterError( float jitterErr )
+    {
+            // use 8 bits (3 exp, 5 mant) and store in ps
+            // has range of 5 ps - 5000 ps
+            // expect input in BX units
+            // all bits off --> time reco bailed out
+            if(jitterErr <= 0)
+            {
+                    aux_ = (~0xFF & aux_);
+                    return;
+            }
+            // all bits on  --> time error over 5 ns
+            if(25*jitterErr >= 5)
+            {
+                    aux_ = (0xFF | aux_);
+                    return;
+            }
+
+            float LSB = 1.26008;
+            float quantityInLSB = (1000*25*jitterErr)/LSB;
+            int log2OfQuantity = (int) (log2( quantityInLSB ));
+            int exponentTmp = log2OfQuantity - 4;
+            uint8_t exponent=0;
+            if (exponentTmp>0) exponent = exponentTmp;
+            uint8_t significand = (int) ( std::lround( quantityInLSB / std::pow(2,exponent) )   );
+            uint32_t jitterErrorBits = exponent<<5 | significand;
+      
+            if( (0xFF & jitterErrorBits) == 0xFF)
+              jitterErrorBits = 0xFE;
+            if( (0xFF & jitterErrorBits) == 0x00)
+              jitterErrorBits = 0x01;
+
+            aux_ = (~0xFF & aux_) | (jitterErrorBits & 0xFF);
+
+    }
+  constexpr void setFlags( uint32_t flags ) { flags_ = flags; }
+  constexpr void setId( DetId id ) { id_ = id; }
+  constexpr void setAux( uint32_t aux ) { aux_ = aux; }
+    constexpr void setFlagBit(EcalUncalibratedRecHit::Flags flag){
+           if  (flag == kGood) {
+              //then set all bits to zero;
+              flags_  = 0;
+              return;
+          }
+         // else set the flagbit
+         flags_|= 0x1 <<  flag;  
+    }
+    constexpr bool checkFlag(EcalUncalibratedRecHit::Flags flag) const {
+           if(flag == kGood){ if ( ! flags_ ) return true;else return false;} // if all flags are unset, then hit is good
+           return  flags_ & ( 0x1<<flag);
+    }
+
+    constexpr bool isSaturated() const {
+      return EcalUncalibratedRecHit::checkFlag(kSaturated);
+    }
+    constexpr bool isJitterValid() const
+    {
+            if(jitterError() <= 0)
+              return false;
+            else
+              return true;
+    }
+    constexpr bool isJitterErrorValid() const
+    {
+            if(!isJitterValid())
+              return false;
+            if(jitterError() >= 10000)
+              return false;
+
+            return true;
+    }
 
  private:
   float amplitude_;           //< Reconstructed amplitude
