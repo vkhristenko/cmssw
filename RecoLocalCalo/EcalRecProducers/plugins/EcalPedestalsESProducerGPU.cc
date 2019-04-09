f987b912182fb55255ab8efb6db2224e91e25421
Viktor Khristenko <vdkhristenko1991@gmail.com>
2019-04-09 19:29:56 +0200

    pedestals with patatrack framework

--- RecoLocalCalo/EcalRecProducers/plugins/EcalPedestalsESProducerGPU.cc
+++ RecoLocalCalo/EcalRecProducers/plugins/EcalPedestalsESProducerGPU.cc
@@ -17,12 +17,16 @@ public:
     
     std::unique_ptr<EcalPedestalsGPU> produce(EcalPedestalsRcd const&);
 
+    static void fillDescriptions(edm::ConfigurationDescriptions& desc);
+
 private:
     std::string pedestalsLabel_;
 };
 
-EcalPedestalsESProducerGPU::EcalPedestalsESProducerGPU(edm::ParameterSet const& ps) {
-    std::string name = ps.getUntrackedParameter<std::string>("ComponentName");
+EcalPedestalsESProducerGPU::EcalPedestalsESProducerGPU(edm::ParameterSet const& ps) 
+    : pedestalsLabel_{ps.getParameter<std::string>("pedestalsLabel")}
+{
+    std::string name = ps.getParameter<std::string>("ComponentName");
     setWhatProduced(this, name);
 }
 
@@ -36,6 +40,16 @@ std::unique_ptr<EcalPedestalsGPU> EcalPedestalsESProducerGPU::produce(
     std::cout << __PRETTY_FUNCTION__ << std::endl;
 
     return std::make_unique<EcalPedestalsGPU>(*pedestals);
+}
+
+void EcalPedestalsESProducerGPU::fillDescriptions(
+        edm::ConfigurationDescriptions& confDesc) {
+    edm::ParameterSetDescription desc;
+
+    std::string label = "ecalPedestalsESProducerGPU";
+    desc.add<std::string>("ComponentName", "");
+    desc.add<std::string>("pedestalsLabel", "")->setComment("Pedestals Label");
+    confDesc.add(label, desc);
 }
 
 DEFINE_FWK_EVENTSETUP_MODULE(EcalPedestalsESProducerGPU);
--- RecoLocalCalo/EcalRecProducers/plugins/EcalPedestalsGPU.h
+++ RecoLocalCalo/EcalRecProducers/plugins/EcalPedestalsGPU.h
@@ -2,15 +2,22 @@
 
 #include "HeterogeneousCore/CUDAUtilities/interface/CUDAHostAllocator.h"
 
-class Pedestals {};
+class Pedestals {
+    float *mean_x12, *mean_x6, *mean_x1;
+    float *rms_x12, *rms_x6, *rms_x1;
+};
 
 class EcalPedestalsGPU {
 public:
     using Product = Pedestals;
 
-    EcalPedestalsGPU(EcalPedestals const&) {}
+    EcalPedestalsGPU(EcalPedestals const&);
 
-private:
+public:
+    // in the future, we need to arrange so to avoid this copy on the host
+    // store eb first then ee
+    // maintain offset
+    int offsetEB;
     std::vector<float, CUDAHostAllocator<float>> mean_x12;
     std::vector<float, CUDAHostAllocator<float>> rms_x12;
     std::vector<float, CUDAHostAllocator<float>> mean_x6;
