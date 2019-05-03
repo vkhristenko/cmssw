219f1863d854da84c7b008c748a4905f51613cb7
Viktor Khristenko <vdkhristenko1991@gmail.com>
2019-05-03 18:40:33 +0200

    add v2 back in

---
+++ RecoLocalCalo/EcalRecAlgos/src/AmplitudeComputationKernelsV2.h
@@ -0,0 +1,36 @@
+#ifndef RecoLocalCalo_EcalRecAlgos_src_AmplitudeComputationKernelsV2
+#define RecoLocalCalo_EcalRecAlgos_src_AmplitudeComputationKernelsV2
+
+#include <iostream>
+#include <limits>
+
+#include "DataFormats/EcalDigi/interface/EcalDataFrame.h"
+#include "RecoLocalCalo/EcalRecAlgos/interface/Common.h"
+
+#include "DataFormats/Math/interface/approx_exp.h"
+#include "DataFormats/Math/interface/approx_log.h"
+
+#include "RecoLocalCalo/EcalRecAlgos/interface/DeclsForKernels.h"
+
+#include "cuda.h"
+
+//#define DEBUG
+
+//#define ECAL_RECO_CUDA_DEBUG
+
+namespace ecal { namespace multifit {
+
+namespace v2 {
+
+void minimization_procedure(
+        EventInputDataCPU const& eventInputCPU, EventInputDataGPU& eventInputGPU,
+        EventOutputDataGPU& eventOutputGPU, EventDataForScratchGPU& scratch,
+        ConditionsProducts const& conditions,
+        ConfigurationParameters const& configParameters,
+        cuda::stream_t<>& cudaStream);
+
+}
+
+}}
+
+#endif // RecoLocalCalo_EcalRecAlgos_src_AmplitudeComputationKernelsV2
