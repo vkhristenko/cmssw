e069518928d183ce540a69d2f52fe91e97d4482d
Viktor Khristenko <vdkhristenko1991@gmail.com>
2019-07-19 10:19:43 +0200

    fixing, producing transient cuda products

--- CUDADataFormats/HcalDigi/interface/DigiCollection.h
+++ CUDADataFormats/HcalDigi/interface/DigiCollection.h
@@ -25,8 +25,25 @@ struct Flavor4 {
 
 struct Flavor5 {
     static constexpr float WORDS_PER_SAMPLE = 0.5;
+    static constexpr int SAMPLES_PER_WORD = 2;
     static constexpr int HEADER_WORDS = 1;
 };
+
+template<typename Flavor>
+constexpr uint32_t compute_stride(uint32_t const nsamples) { 
+    return static_cast<uint32_t>(nsamples * Flavor::WORDS_PER_SAMPLE) + 
+        Flavor::HEADER_WORDS;
+}
+
+template<typename Flavor>
+constexpr uint32_t compute_nsamples(uint32_t const nwords) {
+    return (nwords - Flavor::HEADER_WORDS) / Flavor::WORDS_PER_SAMPLE;
+}
+
+template<>
+constexpr uint32_t compute_nsamples<Flavor5>(uint32_t const nwords) {
+    return (nwords - Flavor5::HEADER_WORDS) * Flavor5::SAMPLES_PER_WORD;
+}
 
 //
 // this is basically a view 
@@ -35,8 +52,8 @@ struct Flavor5 {
 template<typename Flavor>
 struct DigiCollection {
     DigiCollection() = default;
-    DigiCollection(uint32_t *ids, uint16_t *data, uint32_t ndigis, uint32_t nsamples)
-        : ids{ids}, data{data}, ndigis{ndigis}, nsamples{nsamples}
+    DigiCollection(uint32_t *ids, uint16_t *data, uint32_t ndigis, uint32_t stride)
+        : ids{ids}, data{data}, ndigis{ndigis}, stride{stride}
     {}
     DigiCollection(DigiCollection const&) = default;
     DigiCollection& operator=(DigiCollection const&) = default;
@@ -48,7 +65,7 @@ struct DigiCollection {
     uint32_t *ids=nullptr;
     uint16_t *data=nullptr;
     uint32_t ndigis;
-    uint32_t nsamples;
+    uint32_t stride;
 };
 
 }
