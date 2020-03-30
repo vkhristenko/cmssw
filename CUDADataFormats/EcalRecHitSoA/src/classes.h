0d118e117ed6f7414f4436139a59310f18eab02b
Viktor Khristenko <vdkhristenko1991@gmail.com>
2020-03-30 15:44:49 +0200

    cuda data formats ecal rechit  fixes for 110x

--- CUDADataFormats/EcalRecHitSoA/src/classes.h
+++ CUDADataFormats/EcalRecHitSoA/src/classes.h
@@ -1,2 +1,3 @@
 #include "DataFormats/Common/interface/Wrapper.h"
+#include "CUDADataFormats/Common/interface/Product.h"
 #include "CUDADataFormats/EcalRecHitSoA/interface/EcalUncalibratedRecHit_soa.h"
